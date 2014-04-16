4baca90f83b150f9360d827fcb65424863d2d8b1
Martin Gostisha <martingostisha@gmail.com>
2014-04-15 22:57:57 -0500

    added player and team shot charts

--- nbagame.py
+++ nbagame.py
@@ -14,16 +14,20 @@ class NBAgame(object):
 		self.date = self.date[4:6] + '/' + self.date[6:8] + '/' + self.date[0:4]
 
 		self.a1, self.a2, self.a3, self.a4, self.a5, self.h1, self.h2, self.h3, \
-		self.h4, self.h5, self.period, self.ime, self.team, self.etype, self.assist, \
+		self.h4, self.h5, self.period, self.time, self.team, self.etype, self.assist, \
 		self.away, self.block, self.entered, self.home, self.left, self.num, \
 		self.opponent, self.outof, self.player, self.points, self.possession, \
 		self.reason, self.result, self.steal, self.kind, self.x, self.y = range(32)
 
 	def printShots(self):
+		count = 0
 
 		for play in self.gamearr:
 			if (play[self.etype] == 'shot'):
 				print play[self.player] + ' ' + play[self.result] + ' a ' + play[self.kind]
+				count += 1
+
+		print count
 
 	def printRebounds(self):
 
@@ -37,17 +41,23 @@ class NBAgame(object):
 			if (play[self.etype] == 'foul'):
 				print play[self.opponent] + ' committed the ' + play[self.kind] + ' foul on ' + play[self.player]
 
-	def plotShots(self):
+	def plotAllShots(self):
 
 		self.xshots = []
 		self.yshots = []
 		self.shotres = []
 
+		halfs = 0
+
 		for play in self.gamearr:
-			if (play[self.etype] == 'shot'):
+			if (play[self.etype] == 'shot' and float(play[self.y]) <= 47.0):
 				self.xshots.append(float(play[self.x]))
 				self.yshots.append(float(play[self.y]))
 				self.shotres.append(play[self.result])
+			elif (play[self.etype] == 'shot' and float(play[self.y]) >= 47.0):
+				halfs += 1
+
+		print 'Shots from beyond halfcourt: ' + str(halfs)
 
 		mades = []
 		misseds = []
@@ -63,11 +73,114 @@ class NBAgame(object):
 		xmisseds = [self.xshots[j] for j in misseds]
 		ymisseds = [self.yshots[j] for j in misseds]
 
-		datafile = cbook.get_sample_data('/Users/Martin/anaconda/NBA/bucksnewcourt.jpg')
+		datafile = cbook.get_sample_data('/Users/Martin/anaconda/NBA/bucksnewcourthalf.jpg')
 		img = misc.imread(datafile)
-		plt.scatter(xmisseds, ymisseds, marker='x', c='r', zorder=2)
-		plt.scatter(xmades, ymades, marker='o', c='g', zorder=1)
-		plt.imshow(img, zorder=0, extent=[0.0, 50.0, 0.0, 94.0])
+		miss = plt.scatter(xmisseds, ymisseds, marker='x', c='r', s=40, zorder=2)
+		make = plt.scatter(xmades, ymades, marker='o', c='g', s=100, zorder=1)
+		plt.imshow(img, zorder=0, extent=[0.0, 50.0, 0.0, 47.0])
+		plt.legend((make, miss), ('Make', 'Miss'), scatterpoints=1, loc='upper right', ncol=1, fontsize=10)
 		plt.show()
 		plt.title(self.ateam + '@' + self.hteam + ' on ' + self.date)
+
+		print len(xmades)+len(xmisseds)
+
+	def plotTeamShots(self, teamcode):
+
+		self.xshotsteam = []
+		self.yshotsteam = []
+		self.shotresteam = []
+
+		halfs = 0
+
+		for play in self.gamearr:
+			if (play[self.etype] == 'shot' and float(play[self.y]) <= 47.0 and play[self.team] == teamcode):
+				self.xshotsteam.append(float(play[self.x]))
+				self.yshotsteam.append(float(play[self.y]))
+				self.shotresteam.append(play[self.result])
+			elif (play[self.etype] == 'shot' and float(play[self.y]) >= 47.0 and play[self.team] == teamcode):
+				halfs += 1
+
+		print 'Shots from ' + teamcode + ' beyond halfcourt: ' + str(halfs)
+
+		mades = []
+		misseds = []
+
+		for i in range(len(self.shotresteam)):
+			if(self.shotresteam[i] == 'made'):
+				mades.append(i)
+			else:
+				misseds.append(i)
+
+		xmades = [self.xshotsteam[i] for i in mades]
+		ymades = [self.yshotsteam[i] for i in mades]
+		xmisseds = [self.xshotsteam[j] for j in misseds]
+		ymisseds = [self.yshotsteam[j] for j in misseds]
+
+		datafile = cbook.get_sample_data('/Users/Martin/anaconda/NBA/bucksnewcourthalf.jpg')
+		img = misc.imread(datafile)
+		miss = plt.scatter(xmisseds, ymisseds, marker='x', c='r', s=40, zorder=2)
+		make = plt.scatter(xmades, ymades, marker='o', c='g', s=100, zorder=1)
+		plt.imshow(img, zorder=0, extent=[0.0, 50.0, 0.0, 47.0])
+		plt.legend((make, miss), ('Make', 'Miss'), scatterpoints=1, loc='upper right', ncol=1, fontsize=10)
+		plt.show()
+		plt.title(teamcode + ' - ' + self.ateam + '@' + self.hteam + ' on ' + self.date)
+
+		print len(xmades)+len(xmisseds)
+
+	def plotPlayerShots(self, shooter):
+
+		self.xshotsplayer = []
+		self.yshotsplayer = []
+		self.shotresplayer = []
+
+		halfs = 0
+
+		for play in self.gamearr:
+			if (play[self.etype] == 'shot' and float(play[self.y]) <= 47.0 and play[self.player] == shooter):
+				self.xshotsplayer.append(float(play[self.x]))
+				self.yshotsplayer.append(float(play[self.y]))
+				self.shotresplayer.append(play[self.result])
+			elif (play[self.etype] == 'shot' and float(play[self.y]) >= 47.0 and play[self.player] == shooter):
+				halfs += 1
+
+		print 'Shots from ' + shooter + ' beyond halfcourt: ' + str(halfs)
+
+		mades = []
+		misseds = []
+
+		for i in range(len(self.shotresplayer)):
+			if(self.shotresplayer[i] == 'made'):
+				mades.append(i)
+			else:
+				misseds.append(i)
+
+		xmades = [self.xshotsplayer[i] for i in mades]
+		ymades = [self.yshotsplayer[i] for i in mades]
+		xmisseds = [self.xshotsplayer[j] for j in misseds]
+		ymisseds = [self.yshotsplayer[j] for j in misseds]
+
+		datafile = cbook.get_sample_data('/Users/Martin/anaconda/NBA/bucksnewcourthalf.jpg')
+		img = misc.imread(datafile)
+		miss = plt.scatter(xmisseds, ymisseds, marker='x', c='r', s=40, zorder=2)
+		make = plt.scatter(xmades, ymades, marker='o', c='g', s=100, zorder=1)
+		plt.imshow(img, zorder=0, extent=[0.0, 50.0, 0.0, 47.0])
+		plt.legend((make, miss), ('Make', 'Miss'), scatterpoints=1, loc='upper right', ncol=1, fontsize=10)
+		plt.show()
+		plt.title(shooter + ' - ' + self.ateam + '@' + self.hteam + ' on ' + self.date)
+
+		print len(xmades)+len(xmisseds)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
